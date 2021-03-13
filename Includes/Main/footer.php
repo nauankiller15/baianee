fc638fbcdc39183d4062006711fef497b27f2dd1
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-13 20:36:51 -0300

    visual renovado

--- Includes/Main/footer.php
+++ Includes/Main/footer.php
@@ -2,7 +2,7 @@
 	<div class="container-footer">
 		<div class="content-left-right">
 			<div data-w-id="24f6793a-b21c-33f7-a18f-14e8f200aedf" class="content-half" style="opacity: 1;">
-				<a href="/" aria-current="page" class="footer-logo w-inline-block w--current">
+				<a href="#" aria-current="page" class="footer-logo w-inline-block w--current">
 					<img src="https://1.bp.blogspot.com/-Vnl1pnrUrvM/YEv9fKIT1yI/AAAAAAAAG0w/v4BSbqbziZIeeOLjQPZ4jUvhmSce9-lOQCLcBGAsYHQ/s0/baiane-site-logo.png" alt=""></a>
 				<h6 class="headline-8">A equipe Baianê está sempre disposta para você.</h6>
 
@@ -11,7 +11,7 @@
 				<div class="top">
 					<div class="footer-title">Entre em contato</div>
 					<div><a href="mailto:baianeagencia@gmail.com?subject=Olá%20Baianê%20">baianeagencia@gmail.com</a></div>
-					<div><a href="tel:+409668998">+55 71 98395-4205</a></div>
+					<div><a href="tel:71 98395-4205">71 98395-4205</a></div>
 				</div>
 				<div class="bottom">
 					<div class="footer-title">Navegação</div>
@@ -25,7 +25,7 @@
 	</div>
 </div>
 <div class="creditos">
-	<p>© COPYRIGHT - Todos os direitos reservados - 2021. Baianê.</p>
+	<p>© COPYRIGHT - Todos os direitos reservados - 2021</p>
 </div>
 
 
--- Includes/Main/outros.php
+++ Includes/Main/outros.php
@@ -1,6 +1,6 @@
 <div id="Depoimentos" class="friends-section">
     <div class="container-small">
-        <h4 style="opacity: 1;" class="center-width">Depoimentos</h4>
+        <h3 style="opacity: 1;" class="center-width">Depoimentos</h3>
         <div class="w-layout-grid logos-grid" style="opacity: 0;"><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c5274eecbb64ba26bb_Logo%2001%402x.png" width="167.5" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c67ff1c630e94e34dd_Logo%2002%402x.png" width="135" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c6fa55484dec00abfc_Logo%2003%402x.png" width="133.5" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c69ba5cb072a43a4d5_Logo%2004%402x.png" width="108" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c6fa55486cb400abfd_Logo%2005%402x.png" width="116" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c6d6f791cde7a63d6b_Logo%2006%402x.png" width="151.5" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c67ff1c64a9d4e34de_Logo%2007%402x.png" width="130.5" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c6274eec917cba26bc_Logo%2008%402x.png" width="146" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c6fa5548372f00abff_Logo%2009%402x.png" width="81" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c6fa5548507600ac00_Logo%2010%402x.png" width="110.5" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c69ba5cb3c4143a4d7_Logo%2011%402x.png" width="130" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c7d6f7911744a63d6d_Logo%2012%402x.png" width="123" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c7fa55488cd500ac01_Logo%2013%402x.png" width="130.5" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c717a0cf5f4fd91942_Logo%2014%402x.png" width="139" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c7d6f79198bea63d6e_Logo%2015%402x.png" width="115" alt=""><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15a0c7d6f79151efa63d6f_Logo%2016%402x.png" width="90.5" alt=""></div>
     </div>
 </div>
@@ -12,13 +12,13 @@
         <div class="content-half">
             <div class="step" style="opacity: 1;">
                 <div class="steps-icon"><img src="https://1.bp.blogspot.com/-ava_qP119Gc/YE1GPn2g1JI/AAAAAAAAG2I/iax8QLR-H3wGtTI1ujAOz92QzMIEFAoZwCLcBGAsYHQ/s320/laptop.png" height="70" alt=""></div>
-                <div class="headline-7">Gestor de Tráfego</div>
+                <div class="headline-7">Gestão de Tráfego</div>
                 <p>Tráfego pago se refere aos visitantes que chegam até um site, 
                     loja ou página por meio de anúncios publicitários em plataformas específicas. ... 
                     Com isso, é preciso usar outras estratégias de marketing digital para voltar a aumentar o número de visitantes ao site em questão.</p>
             </div>
             <div class="step" style="opacity: 1;">
-                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-By1fRF1MNzE/YE1GQXnjNwI/AAAAAAAAG2M/5jrYzYgjGj0jEOGjgck7cXUXGdFzouSQwCLcBGAsYHQ/s320/social-media.png" height="60" alt=""></div>
+                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-By1fRF1MNzE/YE1GQXnjNwI/AAAAAAAAG2M/5jrYzYgjGj0jEOGjgck7cXUXGdFzouSQwCLcBGAsYHQ/s320/social-media.png" height="70" alt=""></div>
                 <div class="headline-7">Social Media</div>
                 <p>
                 O social media é responsável por atualizar, 
@@ -27,7 +27,7 @@
                 </p>
             </div>
             <div class="step" style="opacity: 1;">
-                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-STnq3ISyBzY/YE1GPskQw3I/AAAAAAAAG2E/DTYncXNNPaw2ee0vYOWQm3D1R5P09BpDwCLcBGAsYHQ/s320/lamp.png" height="66" alt=""></div>
+                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-STnq3ISyBzY/YE1GPskQw3I/AAAAAAAAG2E/DTYncXNNPaw2ee0vYOWQm3D1R5P09BpDwCLcBGAsYHQ/s320/lamp.png" height="70" alt=""></div>
                 <div class="headline-7">Mentoria</div>
                 <p>A mentoria serve para ajudar profissionais na construção e 
                     direcionamento da carreira. ... O objetivo da mentoria é fornecer conhecimentos 
@@ -36,7 +36,7 @@
                     Durante o processo, o mentor atua como fonte de inspiração e referência.</p>
             </div>
             <div class="step" style="opacity: 1;">
-                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-oS7Jf4fZu5Y/YE1GQyjcj8I/AAAAAAAAG2Q/A-Ind1xQ31EH68H8z_OuD83BgGP3Y30dQCLcBGAsYHQ/s320/video.png" height="69" alt=""></div>
+                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-oS7Jf4fZu5Y/YE1GQyjcj8I/AAAAAAAAG2Q/A-Ind1xQ31EH68H8z_OuD83BgGP3Y30dQCLcBGAsYHQ/s320/video.png" height="70" alt=""></div>
                 <div class="headline-7">Videos Institucionais</div>
                 <p>O propósito do Vídeo Institucional é, no fim das contas, 
                     criar uma boa imagem e contar uma história a respeito da sua empresa, 
@@ -44,13 +44,13 @@
                     público interno, enfim, os públicos que forem do seu interesse.</p>
             </div>
             <div class="step last" style="opacity: 1;">
-                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-smpmHcEB5fA/YE1GPnzOKqI/AAAAAAAAG2A/lR9-TApToBQ278Dpg4X_hyYisFjbDFH8ACLcBGAsYHQ/s320/camera.png" height="56" alt=""></div>
+                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-smpmHcEB5fA/YE1GPnzOKqI/AAAAAAAAG2A/lR9-TApToBQ278Dpg4X_hyYisFjbDFH8ACLcBGAsYHQ/s320/camera.png" height="70" alt=""></div>
                 <div class="headline-7">Fotografia</div>
                 <p> A fotografia é mais do que um registro é um bem precioso quando está 
                     relacionado a um fato histórico. Nos ajuda a entender o mundo em diversas óticas.</p>
             </div>
             <div class="step last" style="opacity: 1;">
-                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-u2CluGvT7KY/YE1GRRxDnAI/AAAAAAAAG2U/au6scZLvPE86oGGpbnyJ-TTL8FMg-MTPQCLcBGAsYHQ/s320/web-design.png" height="56" alt=""></div>
+                <div class="steps-icon"><img src="https://1.bp.blogspot.com/-u2CluGvT7KY/YE1GRRxDnAI/AAAAAAAAG2U/au6scZLvPE86oGGpbnyJ-TTL8FMg-MTPQCLcBGAsYHQ/s320/web-design.png" height="70" alt=""></div>
                 <div class="headline-7">Criação de Sites</div>
                 <p>Além de credibilidade, um site é a principal forma para se 
                     estabelecer um bom relacionamento com o consumidor, 
