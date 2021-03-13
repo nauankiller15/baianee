631efa8def1bbb37159201288eaaf15b06ae5c35
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-13 01:56:14 -0300

    visual ajustado da homepage

--- Includes/Main/Utilitarios/video.php
+++ Includes/Main/Utilitarios/video.php
@@ -1,25 +1,7 @@
-<div class="menu-block" style="transform: translate3d(0px, 0px, 0px) scale3d(0, 0, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d; display: flex; opacity: 0;">
-    <div class="menu-wrap">
-        <div class="menu-links"><a href="/about" class="link-heading-menu w-inline-block">
-                <h2 class="heading-menu" style="transform: translate3d(-20px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"><span class="menu-span" style="opacity: 0;">01</span>About</h2>
-            </a><a href="/services" class="link-heading-menu w-inline-block">
-                <h2 class="heading-menu" style="transform: translate3d(-20px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"><span class="menu-span" style="opacity: 0;">02</span>Services</h2>
-            </a><a href="/portfolio" class="link-heading-menu w-inline-block">
-                <h2 class="heading-menu" style="transform: translate3d(-20px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"><span class="menu-span" style="opacity: 0;">03</span>Portfolio</h2>
-            </a><a href="/contact-us" class="link-heading-menu w-inline-block">
-                <h2 class="heading-menu" style="transform: translate3d(-20px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"><span class="menu-span" style="opacity: 0;">04</span>Contact</h2>
-            </a></div>
-        <div class="menu-contact">
-            <div class="menu-circles"><img src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e15d5b20711e61995d8d50a_menu%20circles.png" alt="" class="image"></div>
-            <div><a href="mailto:hello@alpha.com?subject=You've%20got%20mail!" class="menu-contact-link">hello@alpha.com</a></div>
-            <div><a href="tel:+409668998" class="menu-contact-link">(+40) 966-8998</a></div>
-        </div>
-    </div>
-</div>
 <div class="sticky-menu">
     <div class="info-left">
         <div>Siga-nos</div>
-        <div class="middle-line"></div><a href="https://www.facebook.com/" target="_blank" class="vertical-link">Fb</a><a href="https://www.linkedin.com/" target="_blank" class="vertical-link">In</a><a href="https://twitter.com/" target="_blank" class="vertical-link">Tw</a>
+        <div class="middle-line"></div><a href="#" target="_blank" class="vertical-link"><i class="fab fa-facebook-f"></i></a><a href="#" target="_blank" class="vertical-link"><i class="fab fa-instagram"></i></a><a href="#" target="_blank" class="vertical-link"><i class="fab fa-twitter"></i></a>
     </div>
     <div class="info-right">
         <div>Salvador - BA</div>
@@ -33,7 +15,14 @@
         <div class="banner-content-half">
             <h1 class="" style="animation:positBrand 1.7s ease-in-out;"><span class="typed"></span></h1>
         </div>
-        <div class="main-links"><a href="/about" class="link w-inline-block">
+    </div>
+    <div data-w-id="f1437061-9af3-a0db-7804-f6c236979f1d" class="mouse-scroll"><img src="https://uploads-ssl.webflow.com/5de4dd7b59d27b84dc3121f6/5df384643b458ad8b70f65d5_moause%20base.svg" alt="" class="mouse-base"><img src="https://uploads-ssl.webflow.com/5de4dd7b59d27b84dc3121f6/5df38464d6a5f90643ca3c93_mouse%20scroll.svg" width="2" data-w-id="f1437061-9af3-a0db-7804-f6c236979f1f" alt="" class="mouse-scroll-weel" style="opacity: 0.6115; transform: translate3d(0px, 3px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d; will-change: opacity;"><a href="#steps" class="mouse-scroll-link w-inline-block"></a></div>
+    <div data-poster-url="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-poster-00001.jpg" data-video-urls="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.mp4,https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="background-video w-background-video w-background-video-atom"><video autoplay="" loop="" style="background-image:url(&quot;https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-poster-00001.jpg&quot;)" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover">
+            <source src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.mp4" data-wf-ignore="true">
+            <source src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.webm" data-wf-ignore="true">
+        </video></div>
+</div>
+<div class="main-links"><a href="/about" class="link w-inline-block">
                 <div>Topico 1</div>
                 <div class="link-underline" style="width: 0%;"></div>
             </a><a data-w-id="646f259f-093c-cbaf-b725-c62cb30d47f9" href="/services" class="link w-inline-block">
@@ -43,10 +32,3 @@
                 <div>Topico 3</div>
                 <div class="link-underline" style="width: 0%; height: 2px;"></div>
             </a></div>
-    </div>
-    <div data-w-id="f1437061-9af3-a0db-7804-f6c236979f1d" class="mouse-scroll"><img src="https://uploads-ssl.webflow.com/5de4dd7b59d27b84dc3121f6/5df384643b458ad8b70f65d5_moause%20base.svg" alt="" class="mouse-base"><img src="https://uploads-ssl.webflow.com/5de4dd7b59d27b84dc3121f6/5df38464d6a5f90643ca3c93_mouse%20scroll.svg" width="2" data-w-id="f1437061-9af3-a0db-7804-f6c236979f1f" alt="" class="mouse-scroll-weel" style="opacity: 0.6115; transform: translate3d(0px, 3px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d; will-change: opacity;"><a href="#steps" class="mouse-scroll-link w-inline-block"></a></div>
-    <div data-poster-url="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-poster-00001.jpg" data-video-urls="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.mp4,https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="background-video w-background-video w-background-video-atom"><video autoplay="" loop="" style="background-image:url(&quot;https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-poster-00001.jpg&quot;)" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover">
-            <source src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.mp4" data-wf-ignore="true">
-            <source src="https://uploads-ssl.webflow.com/5df8a318143ebc0bdb0bfba6/5e144b86a23876981d914d1e_Agency-transcode.webm" data-wf-ignore="true">
-        </video></div>
-</div>
