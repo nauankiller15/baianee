722b9253b21cc1375b063038f7301608dcff76a7
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-14 20:24:40 -0300

    videos postados edit 2

--- Includes/Main/Topicos/Depoimentos.php
+++ Includes/Main/Topicos/Depoimentos.php
@@ -1,40 +1,10 @@
-<div id="Depoimentos">
+<div id="Depoimentos" data-aos="zoom-in-up" data-aos-duration="900">
     <div class="friends-section">
-        <div class="container-depoimentos">
+        <div class="container-small">
             <h3 style="opacity: 1;" class="center-width">Depoimentos</h3>
+
             <!--  -->
-            <div class="w-gallery">
-                <section id="responsiveGallery-container" class="responsiveGallery-container">
-                    <a class="responsiveGallery-btn responsiveGallery-btn_prev" href="javascript:void(0);"></a>
-                    <a class="responsiveGallery-btn responsiveGallery-btn_next" href="javascript:void(0);"></a>
-                    <ul class="responsiveGallery-wrapper">
-                        <li id="1" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/SFpT1fBqByE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                        <li id="2" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/xjojbSVic1U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                        <li id="3" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/Ix80BPV8EnI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                        <li id="4" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/Np7ycrMoKaU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                        <li id="5" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/keJTY93vD-Q" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                        <li id="6" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/WlZNpHjm02k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                        <li id="7" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/9ou7Ib4oz_M" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                        <li id="8" class="responsiveGallery-item">
-                            <iframe width="230" height="450" src="https://www.youtube.com/embed/Lh928IPdjdE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </li>
-                    </ul>
-                </section>
-            </div>
+            
             <!--  -->
 
         </div>
