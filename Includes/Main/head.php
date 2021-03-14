01945442bf7bbd28ca0192def22c683d1816473b
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-14 20:44:18 -0300

    edit site

--- Includes/Main/head.php
+++ Includes/Main/head.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSS\Main\styleindex.css">
     <link rel="stylesheet" type="text/css" href="CSS\Main\footer.css">
@@ -18,6 +19,7 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <!--  -->
     <link rel="icon" href="https://1.bp.blogspot.com/--rVu94FrjTw/YE1lz5boezI/AAAAAAAAG24/z1F36EtHsjMTHqHM5xJpRs0vNcDUDYr3gCLcBGAsYHQ/s0/favicon.ico" type="image/ico">
+    
 
     <title>#VemPraBaianê</title>
 </head>
