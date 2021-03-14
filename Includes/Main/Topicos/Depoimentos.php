2db5af45dcea3c4cc1bea2fd0d689b03261cad62
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-14 20:00:42 -0300

    videos postados edit 2

--- Includes/Main/Topicos/Depoimentos.php
+++ Includes/Main/Topicos/Depoimentos.php
@@ -1,6 +1,6 @@
 <div id="Depoimentos" data-aos="zoom-in-up" data-aos-duration="900">
     <div class="friends-section">
-        <div class="container-small">
+        <div class="container-depoimentos">
             <h3 style="opacity: 1;" class="center-width">Depoimentos</h3>
 
             <!--  -->
