e6d6b6fd963619169b24987da0e4fe5a16d2315c
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-14 20:52:55 -0300

    meta tag

--- Includes/Main/head.php
+++ Includes/Main/head.php
@@ -3,8 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" ontent="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="CSS\Main\responsivo.css">
     <link rel="stylesheet" type="text/css" href="CSS\Main\styleindex.css">
     <link rel="stylesheet" type="text/css" href="CSS\Main\footer.css">
     <link rel="stylesheet" type="text/css" href="CSS\Main\Utilidades\menu.css">
