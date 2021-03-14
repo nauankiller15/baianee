33cc7fddb5e0985153af866d9daeebeb5446b60f
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-14 01:09:25 -0300

    bug consertado na tela de servicos

--- Includes/Main/Topicos/Depoimentos.php
+++ Includes/Main/Topicos/Depoimentos.php
@@ -1,9 +1,7 @@
-<div data-aos="zoom-in-left" data-aos-duration="900">
+<div data-aos="zoom-in-up" data-aos-duration="900">
     <div id="Depoimentos" class="friends-section">
         <div class="container-small">
-            <div data-aos="fade-up-left">
-                <h3 style="opacity: 1;" class="center-width">Depoimentos</h3>
-            </div>
+            <h3 style="opacity: 1;" class="center-width">Depoimentos</h3>
 
             <!--  -->
             <div class="swiper-container">
