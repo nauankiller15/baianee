9286bab1fbf4a340bda6fffb0df667ec118eeb99
nauankiller15 <nauan.alcantara1@gmail.com>
2021-03-14 21:21:45 -0300

    menu alterado mobile

--- Includes/Main/menu.php
+++ Includes/Main/menu.php
@@ -7,6 +7,7 @@
                   <a href="index.php"><li data-item='Inicio'>Inicio</li></a>
                   <li data-item='Sobre'>Sobre</li>
                   <li data-item='Time'>Time</li>
+                  <li class="contato-mobile" data-item='Contato'>Contato</li>
                   <s class="consulta" href='#'><span>Contato</span></s>
               </ul>
           </nav>
