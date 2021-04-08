33f5a00aa43534bec7378a5c54563a4ff6c5f3bf
nauankiller15 <nauan.alcantara1@gmail.com>
2021-04-07 23:31:55 -0300

    novo estilo do time

--- Includes/Paginas/Inicio.php
+++ Includes/Paginas/Inicio.php
@@ -119,7 +119,38 @@
         <div class=" friends-section">
             <div class="container-small">
                 <h3 style="opacity: 1;" class="margem-depo center-width">Depoimentos</h3>
-                
+                <div class="w-gallery">
+                    <section id="responsiveGallery-container" class="responsiveGallery-container">
+                        <a class="responsiveGallery-btn responsiveGallery-btn_prev" href="javascript:void(0);"></a>
+                        <a class="responsiveGallery-btn responsiveGallery-btn_next" href="javascript:void(0);"></a>
+                        <ul class="responsiveGallery-wrapper">
+                            <li id="1" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/SFpT1fBqByE?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                            <li id="2" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/xjojbSVic1U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                            <li id="3" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/Lh928IPdjdE?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                            <li id="4" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/Ix80BPV8EnI?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                            <li id="5" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/Np7ycrMoKaU?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                            <li id="6" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/keJTY93vD-Q?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                            <li id="7" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/WlZNpHjm02k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                            <li id="8" class="responsiveGallery-item">
+                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/9ou7Ib4oz_M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            </li>
+                        </ul>
+                    </section>
+                </div>
                 <div data-aos="fade-right" data-aos-duration="600" class="depoimentos-caixa">
                     <ul>Nós Agradeçemos sua Preferência</ul>
                 </div>
--- Includes/Paginas/Time.php
+++ Includes/Paginas/Time.php
@@ -9,7 +9,7 @@
                             <img src="https://1.bp.blogspot.com/-B-EHuCqqLLM/YFAfd21BqwI/AAAAAAAAG6c/0evFewMdv68FBrH6KOv7LSlrX6nu36xGQCLcBGAsYHQ/s0/Ricardo.png">
                         </div>
                         <h1 class="preto upper margem-titulo">Ricardo</h1>
-                        <span>CEO & Designer Criativo</span>
+                        <span>CEO & Designer Executivo</span>
                     </div>
                 </li>
                 <!--  -->
@@ -19,13 +19,13 @@
                             <img src="https://1.bp.blogspot.com/-B6A6yRd-fgE/YG5Xqhmv6tI/AAAAAAAAG-A/2o4_wu0GDC0VFQO8ln4Y9YMFPEN2CqNeACLcBGAsYHQ/s0/Marcos.png">
                         </div>
                         <h1 class="preto upper margem-titulo">Marcos</h1>
-                        <span>CEO & Designer, Social Media e Dir. Criativo</span>
+                        <span>CEO & Dir. Criativo</span>
                     </div>
 
                 </li>
                 <!--  -->
                 <li>
-                    <div class="erick">
+                    <div data-aos="fade-left" data-aos-duration="900" class="erick">
                         <div class="profile-picture landscape">
                             <img src="https://1.bp.blogspot.com/-3nJA7mn6EEE/YFAflYQ5A3I/AAAAAAAAG6g/uacxEsyUnxMk2BpdxO82VGazR2DToGP0QCLcBGAsYHQ/s0/Erick.png">
                         </div>
@@ -36,7 +36,7 @@
                 </li>
                 <!--  -->
                 <li>
-                    <div class="gabriel-batata">
+                    <div data-aos="fade-right" data-aos-duration="900" class="gabriel-batata">
                         <div class="profile-picture landscape">
                             <img src="https://1.bp.blogspot.com/-8iguvvYF9to/YFAfxLHCelI/AAAAAAAAG6s/tPAu7_zTXXMCI40-V2l2edIGg29pTVjXwCLcBGAsYHQ/s0/Gabriel%2BBatata.png">
                         </div>
@@ -47,7 +47,7 @@
                 </li>
                 <!--  -->
                 <li>
-                    <div class="nauan">
+                    <div data-aos="zoom-in-up" data-aos-duration="900" class="nauan">
                         <div class="profile-picture landscape">
                             <img src="https://1.bp.blogspot.com/-H3DhuUH7F60/YFAf2BtsL_I/AAAAAAAAG6w/-ly4SbXUveAwweDwES7iwu1P6EePAFzjACLcBGAsYHQ/s0/Nauan.png">
                         </div>
