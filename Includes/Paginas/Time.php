6dc2ffc0c66de773ac00c492db1510be7358605f
nauankiller15 <nauan.alcantara1@gmail.com>
2021-04-09 20:38:54 -0300

    alteracao visual na pagina de time

--- Includes/Paginas/Time.php
+++ Includes/Paginas/Time.php
@@ -8,9 +8,9 @@
                         <div class="profile-picture landscape">
                             <img src="https://1.bp.blogspot.com/-RzqmEF2v1N0/YG-0abU-BCI/AAAAAAAAG_w/Sj3an4jYWF4wAnOsr8E6AS_LoLXGF74IACLcBGAsYHQ/s0/Ricardo.png">
                         </div>
+                        <i class="tag-job job1 fas fa-briefcase"></i>
                         <h1 class="preto upper margem-titulo">Ricardo</h1>
                         <span>CEO & Diretor Executivo</span>
-                        <i class="tag-job job1 fas fa-briefcase"></i>
                     </div>
                 </li>
                 <!--  -->
@@ -19,9 +19,9 @@
                         <div class="profile-picture landscape">
                             <img src="https://1.bp.blogspot.com/-B6A6yRd-fgE/YG5Xqhmv6tI/AAAAAAAAG-A/2o4_wu0GDC0VFQO8ln4Y9YMFPEN2CqNeACLcBGAsYHQ/s0/Marcos.png">
                         </div>
+                        <i class="tag-job job2 fas fa-briefcase"></i>
                         <h1 class="preto upper margem-titulo">Marcos</h1>
                         <span>CEO & Diretor Criativo</span>
-                        <i class="tag-job job2 fas fa-briefcase"></i>
                     </div>
 
                 </li>
@@ -31,9 +31,9 @@
                         <div class="profile-picture landscape">
                             <img src="https://1.bp.blogspot.com/-3nJA7mn6EEE/YFAflYQ5A3I/AAAAAAAAG6g/uacxEsyUnxMk2BpdxO82VGazR2DToGP0QCLcBGAsYHQ/s0/Erick.png">
                         </div>
+                        <i class="tag-job job3 fas fa-chart-line"></i>
                         <h1 class="preto upper margem-titulo">Erick</h1>
                         <span>Gestor de Tráfego</span>
-                        <i class="tag-job job3 fas fa-chart-line"></i>
                     </div>
 
                 </li>
@@ -43,9 +43,9 @@
                         <div class="profile-picture landscape">
                             <img src="https://1.bp.blogspot.com/-NJuOUKLkBsg/YG-3ky4GGTI/AAAAAAAAHAI/r6ifVYdVS9MxTxi9fJAb7hq6RkZxfPkiACLcBGAsYHQ/s0/Gabriel%2BBatata.png">
                         </div>
+                        <i class="tag-job job4 fas fa-play-circle"></i>
                         <h1 class="preto upper margem-titulo">Gabriel Batata</h1>
                         <span>Editor Audiovisual</span>
-                        <i class="tag-job job4 fas fa-headphones"></i>
                     </div>
 
                 </li>
@@ -55,9 +55,9 @@
                         <div class="profile-picture landscape nauan-scape">
                             <img src="https://1.bp.blogspot.com/-dQ6_mzhqOaI/YG-fJzt5-II/AAAAAAAAG_I/vEnvCICK69QR1NBsv-JwOtqvaxjU5mHTwCLcBGAsYHQ/s0/Nauan.png">
                         </div>
+                        <i class="tag-job job5 fas fa-laptop-code"></i>
                         <h1 class="preto upper margem-titulo">Nauan</h1>
                         <span class="margem-hud">Web Designer</span>
-                        <i class="tag-job job5 fas fa-laptop-code"></i>
                     </div>
                     <!--  -->
                 </li>
@@ -66,9 +66,9 @@
                         <div class="profile-picture landscape joao-scape">
                             <img src="https://1.bp.blogspot.com/-YsRBcYqMvfc/YG-QFpnGdbI/AAAAAAAAG-Y/BEwr2KqXkU0wCr5Dg-zJTsJxD4uJKaa7gCLcBGAsYHQ/s0/Jo%25C3%25A3o%2BCaetano.png">
                         </div>
+                        <i class="tag-job job6 fas fa-comments"></i>
                         <h1 class="preto upper margem-titulo">João Caetano</h1>
                         <span>Pesquisador & Redator</span>
-                        <i class="tag-job job6 fas fa-comments"></i>
                     </div>
                     <!--  -->
                 </li>
