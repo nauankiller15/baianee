6a5137bae6fd38dbcf4983ef00c72e49cb32db2d
nauankiller15 <nauan.alcantara1@gmail.com>
2021-04-14 23:32:38 -0300

    alteracao topico depoimentos

--- Includes/Paginas/Inicio.php
+++ Includes/Paginas/Inicio.php
@@ -125,7 +125,7 @@
                         <a class="responsiveGallery-btn responsiveGallery-btn_next" href="javascript:void(0);"></a>
                         <ul class="responsiveGallery-wrapper">
                             <li id="1" class="responsiveGallery-item">
-                                <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/SFpT1fBqByE?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <iframe class="respo.container-small {nsivo-depoimentos" src="https://www.youtube.com/embed/SFpT1fBqByE?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </li>
                             <li id="2" class="responsiveGallery-item">
                                 <iframe class="responsivo-depoimentos" src="https://www.youtube.com/embed/xjojbSVic1U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
