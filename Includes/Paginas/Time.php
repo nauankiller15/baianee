a688fcbac78a740e1b52f1271a79c52a604c0b3a
nauankiller15 <nauan.alcantara1@gmail.com>
2021-04-20 22:53:24 -0300

    ajuste posicao background tela de time

--- Includes/Paginas/Time.php
+++ Includes/Paginas/Time.php
@@ -1,77 +1,81 @@
-<div id="time" class="section-depo">
-    <div class="container-big-depoimentos">
-        <h5 class="plus-margin-bottom centro-depo preto">TIME BAIANÊ</h5>
-        <div class="content-width-depoimentos">
-            <div class="depoimento-hud">
-                <li>
-                    <div class="ricardo">
-                        <div class="profile-picture landscape">
-                            <img src="https://1.bp.blogspot.com/-RzqmEF2v1N0/YG-0abU-BCI/AAAAAAAAG_w/Sj3an4jYWF4wAnOsr8E6AS_LoLXGF74IACLcBGAsYHQ/s0/Ricardo.png">
-                        </div>
-                        <i class="tag-job job1 fas fa-briefcase js-scroll slide-left"></i>
-                        <h1 class="preto upper margem-titulo js-scroll slide-right">Ricardo</h1>
-                        <span class="js-scroll slide-left">CEO & Diretor Executivo</span>
-                    </div>
-                </li>
-                <!--  -->
-                <li>
-                    <div class="marcos js-scroll fade-in-bottom-mobile">
-                        <div class="profile-picture landscape">
-                            <img src="https://1.bp.blogspot.com/-B6A6yRd-fgE/YG5Xqhmv6tI/AAAAAAAAG-A/2o4_wu0GDC0VFQO8ln4Y9YMFPEN2CqNeACLcBGAsYHQ/s0/Marcos.png">
-                        </div>
-                        <i class="tag-job job2 fas fa-briefcase js-scroll slide-left"></i>
-                        <h1 class="preto upper margem-titulo js-scroll slide-right">Marcos</h1>
-                        <span class="js-scroll slide-left">CEO & Diretor Criativo</span>
-                    </div>
+<div id="time">
+<div class="menutop"></div>
 
-                </li>
-                <!--  -->
-                <li>
-                    <div class="erick js-scroll">
-                        <div class="profile-picture landscape">
-                            <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-3nJA7mn6EEE/YFAflYQ5A3I/AAAAAAAAG6g/uacxEsyUnxMk2BpdxO82VGazR2DToGP0QCLcBGAsYHQ/s0/Erick.png">
+    <div class="section-depo">
+        <div class="container-big-depoimentos">
+            <h5 class="plus-margin-bottom centro-depo preto">TIME BAIANÊ</h5>
+            <div class="content-width-depoimentos">
+                <div class="depoimento-hud">
+                    <li>
+                        <div class="ricardo">
+                            <div class="profile-picture landscape">
+                                <img src="https://1.bp.blogspot.com/-RzqmEF2v1N0/YG-0abU-BCI/AAAAAAAAG_w/Sj3an4jYWF4wAnOsr8E6AS_LoLXGF74IACLcBGAsYHQ/s0/Ricardo.png">
+                            </div>
+                            <i class="tag-job job1 fas fa-briefcase js-scroll slide-left"></i>
+                            <h1 class="preto upper margem-titulo js-scroll slide-right">Ricardo</h1>
+                            <span class="js-scroll slide-left">CEO & Diretor Executivo</span>
                         </div>
-                        <i class="tag-job job3 fas fa-chart-line js-scroll slide-left"></i>
-                        <h1 class="preto upper margem-titulo js-scroll slide-right">Erick</h1>
-                        <span class="js-scroll slide-right">Gestor de Tráfego</span>
-                    </div>
-
-                </li>
-                <!--  -->
-                <li>
-                    <div class="gabriel-batata js-scroll">
-                        <div class="profile-picture landscape">
-                            <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-NJuOUKLkBsg/YG-3ky4GGTI/AAAAAAAAHAI/r6ifVYdVS9MxTxi9fJAb7hq6RkZxfPkiACLcBGAsYHQ/s0/Gabriel%2BBatata.png">
-                        </div>
-                        <i class="tag-job job4 fas fa-play-circle js-scroll slide-left"></i>
-                        <h1 class="preto upper margem-titulo js-scroll slide-right">Gabriel Batata</h1>
-                        <span class="js-scroll slide-left">Editor Audiovisual</span>
-                    </div>
-
-                </li>
-                <!--  -->
-                <li>
-                    <div class="nauan js-scroll">
-                        <div class="profile-picture landscape nauan-scape">
-                            <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-dQ6_mzhqOaI/YG-fJzt5-II/AAAAAAAAG_I/vEnvCICK69QR1NBsv-JwOtqvaxjU5mHTwCLcBGAsYHQ/s0/Nauan.png">
-                        </div>
-                        <i class="tag-job job5 fas fa-laptop-code js-scroll slide-left"></i>
-                        <h1 class="preto upper margem-titulo js-scroll slide-right">Nauan</h1>
-                        <span class="margem-hud js-scroll slide-left">Web Designer</span>
-                    </div>
+                    </li>
                     <!--  -->
-                </li>
-                <li>
-                    <div class="joao-caetano js-scroll">
-                        <div class="profile-picture landscape joao-scape">
-                            <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-YsRBcYqMvfc/YG-QFpnGdbI/AAAAAAAAG-Y/BEwr2KqXkU0wCr5Dg-zJTsJxD4uJKaa7gCLcBGAsYHQ/s0/Jo%25C3%25A3o%2BCaetano.png">
+                    <li>
+                        <div class="marcos js-scroll fade-in-bottom-mobile">
+                            <div class="profile-picture landscape">
+                                <img src="https://1.bp.blogspot.com/-B6A6yRd-fgE/YG5Xqhmv6tI/AAAAAAAAG-A/2o4_wu0GDC0VFQO8ln4Y9YMFPEN2CqNeACLcBGAsYHQ/s0/Marcos.png">
+                            </div>
+                            <i class="tag-job job2 fas fa-briefcase js-scroll slide-left"></i>
+                            <h1 class="preto upper margem-titulo js-scroll slide-right">Marcos</h1>
+                            <span class="js-scroll slide-left">CEO & Diretor Criativo</span>
                         </div>
-                        <i class="tag-job job6 fas fa-comments js-scroll slide-left"></i>
-                        <h1 class="preto upper margem-titulo js-scroll slide-right">João Caetano</h1>
-                        <span class="js-scroll slide-right">Pesquisador & Redator</span>
-                    </div>
+
+                    </li>
                     <!--  -->
-                </li>
+                    <li>
+                        <div class="erick js-scroll">
+                            <div class="profile-picture landscape">
+                                <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-3nJA7mn6EEE/YFAflYQ5A3I/AAAAAAAAG6g/uacxEsyUnxMk2BpdxO82VGazR2DToGP0QCLcBGAsYHQ/s0/Erick.png">
+                            </div>
+                            <i class="tag-job job3 fas fa-chart-line js-scroll slide-left"></i>
+                            <h1 class="preto upper margem-titulo js-scroll slide-right">Erick</h1>
+                            <span class="js-scroll slide-right">Gestor de Tráfego</span>
+                        </div>
+
+                    </li>
+                    <!--  -->
+                    <li>
+                        <div class="gabriel-batata js-scroll">
+                            <div class="profile-picture landscape">
+                                <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-NJuOUKLkBsg/YG-3ky4GGTI/AAAAAAAAHAI/r6ifVYdVS9MxTxi9fJAb7hq6RkZxfPkiACLcBGAsYHQ/s0/Gabriel%2BBatata.png">
+                            </div>
+                            <i class="tag-job job4 fas fa-play-circle js-scroll slide-left"></i>
+                            <h1 class="preto upper margem-titulo js-scroll slide-right">Gabriel Batata</h1>
+                            <span class="js-scroll slide-left">Editor Audiovisual</span>
+                        </div>
+
+                    </li>
+                    <!--  -->
+                    <li>
+                        <div class="nauan js-scroll">
+                            <div class="profile-picture landscape nauan-scape">
+                                <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-dQ6_mzhqOaI/YG-fJzt5-II/AAAAAAAAG_I/vEnvCICK69QR1NBsv-JwOtqvaxjU5mHTwCLcBGAsYHQ/s0/Nauan.png">
+                            </div>
+                            <i class="tag-job job5 fas fa-laptop-code js-scroll slide-left"></i>
+                            <h1 class="preto upper margem-titulo js-scroll slide-right">Nauan</h1>
+                            <span class="margem-hud js-scroll slide-left">Web Designer</span>
+                        </div>
+                        <!--  -->
+                    </li>
+                    <li>
+                        <div class="joao-caetano js-scroll">
+                            <div class="profile-picture landscape joao-scape">
+                                <img class="js-scroll slide-left" src="https://1.bp.blogspot.com/-YsRBcYqMvfc/YG-QFpnGdbI/AAAAAAAAG-Y/BEwr2KqXkU0wCr5Dg-zJTsJxD4uJKaa7gCLcBGAsYHQ/s0/Jo%25C3%25A3o%2BCaetano.png">
+                            </div>
+                            <i class="tag-job job6 fas fa-comments js-scroll slide-left"></i>
+                            <h1 class="preto upper margem-titulo js-scroll slide-right">João Caetano</h1>
+                            <span class="js-scroll slide-right">Pesquisador & Redator</span>
+                        </div>
+                        <!--  -->
+                    </li>
+                </div>
             </div>
         </div>
     </div>
